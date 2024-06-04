b02819d58d40cef3d7f8e7659e538580ec0d1e20
heidar.dev <heidar.dev@gmail.com>
2024-06-04 10:19:53 +0330

    docs: signup.php

---
+++ php/signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>signup</title>
+    <style>
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            height: 100vh;
+            overflow: hidden;
+        }
+        input{
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <form action="#" method="post">
+        <input type="text" name="username" placeholder="username" class="username">
+        <input type="password" placeholder="password" name="username">
+        <input type="button" value="signup">
+        <input type="button" value="login">
+    </form>
+</body>
+</html>
