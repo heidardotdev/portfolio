c01988ebfca2cf5e4390f4a6cad6d06453cb16eb
heidar.dev <heidar.dev@gmail.com>
2024-06-04 10:17:37 +0330

    feat: coding config.php

--- php/config.php
+++ php/config.php
@@ -2,6 +2,17 @@
 // connnect to the data base config
 
     $db_name='http://localhost:8080/phpmyadmin/index.php?route=/database/structure&db=portfolio';
-
+    $user_password = '';
+    $conn = new PDO($db_name, $user_name, $user_password);
+    function random_id(){
+    $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $rand= array();
+    $length strlen($str)-1;
+    for($i = 0; $i < 20; $i++){ 
+        $n = mt_rand (0, $length)
+        $rand[] = $str[$n];
+    }
+    return implode($rand);
+}
 
 ?>
