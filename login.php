1683898294221fde279c4c21d8e3f4a349869ec0
heidar.dev <heidar.dev@gmail.com>
2024-06-15 12:12:27 +0330

    update

--- login.php
+++ login.php
@@ -1,6 +1,33 @@
 <?php
+include 'db_connect.php';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT id, username, password, image FROM users WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    if ($user && password_verify($password, $user['password'])) {
+        // Password is correct, so start a new session
+        session_start();
+        $_SESSION['loggedin'] = true;
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['image'] = $user['image']; // Store image name in session
+        header("location: index.php"); // Redirect user to welcome page
+    } else {
+        echo "Invalid email or password";
+    }
+    $stmt->close();
+    $conn->close();
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
